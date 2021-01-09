d2ec98bb03c1dac6f5dd7ea67e19fe9980a03641
Ubuntu <hyrumk@kaist.ac.kr>
2021-01-09 14:45:07 +0000

    Add Match Delete

--- routes/matches.js
+++ routes/matches.js
@@ -1,3 +1,6 @@
+//In kotlin, use get to verify if info of the fb_id exist BEFORE POSTING!
+// Might cause conflicts
+
 module.exports = function(app, Match){
     var express = require('express');
     var router = express.Router();
@@ -14,8 +17,16 @@ module.exports = function(app, Match){
 
 
 
+    router.get('/fb_id/:fb_id', function(req, res, next){
+        Match.findOne({fb_id: req.params.fb_id}, function(err, match){
+            if(err) return res.status(500).json({error: err});
+            if(!match) return res.status(404).json({error: 'fb_id not found'});
+            res.json(match);
+        })
+    });
+
     router.get('/own_phone/:own_phone', function(req, res, next){
-        Match.findOne({_id: req.params.own_phone}, function(err, match){
+        Match.findOne({own_phone: req.params.own_phone}, function(err, match){
             if(err) return res.status(500).json({error: err});
             if(!match) return res.status(404).json({error: 'phone number not found'});
             res.json(match);
@@ -23,14 +34,14 @@ module.exports = function(app, Match){
     });
 
     router.get('/own_kakao/:own_kakao', function(req, res, next){
-        Match.findOne({_id: req.params.own_kakao}, function(err, match){
+        Match.findOne({own_kakao: req.params.own_kakao}, function(err, match){
             if(err) return res.status(500).json({error: err});
             if(!match) return res.status(404).json({error: 'kakao id not found'});
             res.json(match);
         })
     });
 
-
+    // POST one's information
     router.post('/', function(req, res, next){
         var match = new Match();
         match.fb_id = req.body.fb_id
@@ -51,8 +62,28 @@ module.exports = function(app, Match){
     });
 
 
+    router.put('/fb_id/:fb_id', function(req, res, next){
+        Match.findOne({fb_id: req.params.fb_id}, function(err, match){
+            console.log(err);
+            if(err) return res.status(500).json({ error: 'database failure' });
+            if(!match) return res.status(404).json({ error: 'match not found' });
+
+            if(req.body.own_phone) match.own_phone = req.body.own_phone;
+            if(req.body.own_kakao) match.own_kakao = req.body.own_kakao;
+            if(req.body.other_phone) match.other_phone = req.body.other_phone;
+            if(req.body.other_kakao) match.other_kakao = req.body.other_kakao;
+
+            match.save(function(err){
+                if(err) res.status(500).json({error: 'failed to update'});
+                res.json({message: 'match updated'});
+            });
+
+        });
+    });
+
+
     router.delete('/fb_id/:fb_id', function(req, res, next){
-        Match.remove({ _id: req.params.fb_id }, function(err, output){
+        Match.remove({fb_id: req.params.fb_id }, function(err, output){
             if(err) return res.status(500).json({ error: "database failure" });
 
             /* ( SINCE DELETE OPERATION IS IDEMPOTENT, NO NEED TO SPECIFY )
