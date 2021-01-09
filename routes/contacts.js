7c15a5bf2f6a3efc943b52c79103d7bee420aa8b
healwon <woni9911@gmail.com>
2021-01-09 13:27:36 +0000

    형식 통일

--- routes/contacts.js
+++ routes/contacts.js
@@ -7,15 +7,19 @@ const Contact = require('../models/contact')
 // get all items
 router.get('/', function(req,res, next){
     Contact.find()
-    .select("name fb_id _id")
+    .select("_id name fb_id")
     .exec(function(err, contacts){
-        if(err) return res.status(500).send({error: 'database failure'});
-        res.json(contacts);
+        if(err) return res.status(500).json({error: err});
+        res.json({
+            resultCode: 1,
+            message: "Success",
+            count: contacts.length,
+            results: contacts});
     })
 });
 
 // get item
-router.get('/:contact_id', function(req, res, next){
+router.get('/item/:contact_id', function(req, res, next){
     Contact.findOne({_id: req.params.contact_id}, function(err, contact){
         if(err) return res.status(500).json({error: err});
         if(!contact) return res.status(404).json({error: 'contact not found'});
@@ -23,15 +27,19 @@ router.get('/:contact_id', function(req, res, next){
     })
 });
 
-// GET BOOK BY fd_id
+// get items by fd_id
 router.get('/fb_id/:fb_id', function(req, res, next){
-    Contact.find({fb_id: req.params.fb_id}, function(err, contacts){
+    Contact.find({fb_id: req.params.fb_id})
+    .select("_id name fb_id")
+    .exec(function(err, contacts){
         if(err) return res.status(500).json({error: err});
-        if(books.length === 0) return res.status(404).json({error: 'book not found'});
-        res.json(contacts);
+        res.json({
+            resultCode: 1,
+            message: "Success",
+            count: contacts.length,
+            results: contacts});
     })
 });
-
 
 // CREATE BOOK
 router.post('/', function(req, res, next){
@@ -52,9 +60,9 @@ router.post('/', function(req, res, next){
 });
 
 // DELETE BOOK
-router.delete('/:contact_id', function(req, res, next){
+router.delete('/item/:contact_id', function(req, res, next){
     contact.remove({ _id: req.params.contact_id }, function(err, output){
-        if(err) return res.status(500).json({ error: "database failure" });
+        if(err) return res.status(500).json({ error: err });
 
         /* ( SINCE DELETE OPERATION IS IDEMPOTENT, NO NEED TO SPECIFY )
         if(!output.result.n) return res.status(404).json({ error: "book not found" });
--- routes/images.js
+++ routes/images.js
@@ -3,8 +3,7 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const multer = require('multer');
 
-SERVER_IP = '192.249.18.162'
-
+SERVER_IP = '192.249.18.54'
 
 var os = require('os');
   
@@ -29,111 +28,91 @@ function getServerIp() {
 const fileFilter = (req, file, cb) => {
     // reject a file
     if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
-      cb(null, true);
+        cb(null, true);
     } else {
-      cb(null, false);
+        cb(null, false);
     }
   }; 
 
-  const storage = multer.diskStorage({
+const storage = multer.diskStorage({
     destination: function(req, file, cb) {
-      cb(null, 'uploads/');
+        cb(null, 'uploads/');
     },
     filename: function(req, file, cb) {
-      cb(null, new Date().toISOString() + file.originalname);
+        cb(null, new Date().toISOString() + file.originalname);
     }
-  });
+});
 
-  const upload = multer({
+const upload = multer({
     storage: storage,
     limits: {
-      fileSize: 1024 * 1024 * 5
+        fileSize: 1024 * 1024 * 5
     },
     fileFilter: fileFilter
-  });
+});
 
-  const Image = require("../models/image");
+const Image = require("../models/image");
 
-
-  
-
-
-
-
-  router.get("/", (req, res, next) => {
+// get all images
+router.get("/", (req, res, next) => {
     Image.find()
-      .select("name fb_id _id userImage")
-      .exec()
-      .then(docs => {
+    .select("_id name userImage fb_id")
+    .exec()
+    .then(docs => {
         const response = {
-          count: docs.length,
-          images: docs.map(doc => {
+            resultCode: 1,
+            message: "Success",
+            count: docs.length,
+            results: docs.map(doc => {
             return {
-              name: doc.name,
-              fb_id: doc.fb_id,
-              userImage: 'http://' + SERVER_IP + ':8080/' + doc.userImage,
-              _id: doc._id,
+                name: doc.name,
+                fb_id: doc.fb_id,
+                userImage: 'http://' + SERVER_IP + ':8080/' + doc.userImage,
+                _id: doc._id,
             };
-          })
+            })
         };
-        //   if (docs.length >= 0) {
-        res.status(200).json(response);
-        //   } else {
-        //       res.status(404).json({
-        //           message: 'No entries found'
-        //       });
-        //   }
-      })
-      .catch(err => {
+      res.status(200).json(response);
+    })
+    .catch(err => {
         console.log(err);
         res.status(500).json({
-          error: err
+            error: err
         });
-      });
-  });
-
-
-  //array로 고치기
-  router.post("/", upload.single('userImage'), (req, res, next) => {
-    const image = new Image({
-      _id: new mongoose.Types.ObjectId(),
-      fb_id: req.body.fb_id,
-      name: req.body.name,
-      userImage: req.file.path
     });
-    image
-      .save()
-      .then(result => {
-        console.log(result);
-        res.status(201).json({
-          message: "Created image successfully",
-          createdImage: {
-              name: result.name,
-              _id: result._id,
-              fb_id: result.fb_id,
-          }
-        });
+});
+
+// get all the images under certain fb_id
+router.get("/fb_id/:fb_id", (req, res, next) => {
+  Image.find({fb_id: req.params.fb_id})
+  .select("_id name userImage fb_id")
+  .exec()
+  .then(docs => {
+    const response = {
+        resultCode: 1,
+        message: "Success",
+        count: docs.length,
+        results: docs.map(doc => {
+        return {
+          name: doc.name,
+          fb_id: doc.fb_id,
+          userImage: 'http://' + SERVER_IP + ':8080/' + doc.userImage,
+          _id: doc._id,
+        };
       })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json({
-          error: err
-        });
-      });
-  });
-
-
-  // get all the images under certain fb_id
-  router.get("/fb_id/:fb_id", (req, res, next) => {
-    Image.find({fb_id: req.params.fb_id}, {_id: 1, name: 1, userImage: 1},  function(err, images){
-      if(err) return res.status(500).json({error: err});
-      if(images.length === 0) return res.status(404).json({error: 'book not found'});
-      res.json(images);
+    };
+    res.status(200).json(response);
   })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json({
+      error: err
+    });
   });
-
+});
+  
 //gets the image by adding /imageID/:imageID at the end.
-  router.get('/imageID/:imageID', function(req, res, next){
+router.get('/item/:imageID', function(req, res, next){
     Image.findOne({_id: req.params.imageID}, function(err, image){
         if(err) return res.status(500).json({error: err});
         if(!image) return res.status(404).json({error: 'image not found'});
@@ -141,10 +120,38 @@ const fileFilter = (req, file, cb) => {
     })
 });
 
+//array로 고치기
+router.post("/", upload.single('userImage'), (req, res, next) => {
+  const image = new Image({
+    _id: new mongoose.Types.ObjectId(),
+    fb_id: req.body.fb_id,
+    name: req.body.name,
+    userImage: req.file.path
+  });
+  image
+    .save()
+    .then(result => {
+      console.log(result);
+      res.status(201).json({
+        message: "Created image successfully",
+        createdImage: {
+            name: result.name,
+            _id: result._id,
+            fb_id: result.fb_id,
+        }
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+});
 
-router.delete("/imageID/:imageId", (req, res, next) => {
-  const id = req.params.imageId;
-  Image.remove({ _id: id })
+router.delete("/item/:imageId", (req, res, next) => {
+    const id = req.params.imageId;
+    Image.remove({ _id: id })
     .exec()
     .then(result => {
       res.status(200).json({
