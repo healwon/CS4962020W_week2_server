7648039633b7a31844376d88d027992c723e6493
Ubuntu <hyrumk@kaist.ac.kr>
2021-01-12 16:24:46 +0000

    Add Attribute

--- routes/matches.js
+++ routes/matches.js
@@ -45,14 +45,45 @@ module.exports = function(app, Match){
         });
     });
 
-
+/*
     router.get('/own_phone/:own_phone', function(req, res, next){
         Match.findOne({own_phone: req.params.own_phone}, function(err, match){
             if(err) return res.status(500).json({error: err});
-            if(!match) return res.status(404).json({error: 'phone number not found'});
+            if(!match) return res.status(200).json({error: 'phone number not found'});
             res.json(match);
         })
     });
+*/
+    
+    router.get('/own_phone/:own_phone', (req, res, next) => {
+        Match.find({own_phone: req.params.own_phone})
+        .select("_id own_phone own_kakao other_phone other_kakao fb_id fb_name")
+        .exec()
+        .then(docs => {
+            const response = {
+                resultCode: 1,
+                message: "Success",
+                count: docs.length,
+                results: docs.map(doc => {
+                    return {
+                        _id: doc._id,
+                        own_phone: doc.own_phone,
+                        own_kakao: doc.own_kakao,
+                        fb_id: doc.fb_id,
+                        fb_name: doc.fb_name,
+                        other_phone: doc.other_phone,
+                        other_kakao: doc.other_kakao
+                    };
+                })
+            };
+            res.status(200).json(response);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({error: err});
+        });
+    });
+
 
     router.get('/own_kakao/:own_kakao', function(req, res, next){
         Match.findOne({own_kakao: req.params.own_kakao}, function(err, match){
